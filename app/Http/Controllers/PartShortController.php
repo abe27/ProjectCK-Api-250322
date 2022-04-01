dcade8708f053b320ed19c1d7d7d26596fbe4c34
Taweechai Yuenyang <krumii.it@gmail.com>
2022-04-01 13:05:23 +0700

    partshort

---
+++ app/Http/Controllers/PartShortController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PartShort;
+use Illuminate\Http\Request;
+
+class PartShortController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PartShort  $partShort
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PartShort $partShort)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PartShort  $partShort
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PartShort $partShort)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PartShort  $partShort
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PartShort $partShort)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PartShort  $partShort
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PartShort $partShort)
+    {
+        //
+    }
+}
---
+++ app/Models/PartShort.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use App\Traits\Nanoids;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Notifications\Notifiable;
+use Laravel\Sanctum\HasApiTokens;
+
+class PartShort extends Model
+{
+    use HasFactory, HasApiTokens, Nanoids, Notifiable;
+
+    protected $fillable = [
+        'order_detail_id',
+        'short_ctn',
+        'is_confirm_short',
+        'is_active',
+    ];
+}
