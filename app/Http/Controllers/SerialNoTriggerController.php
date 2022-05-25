8f33adb3554ec1e93b2c1801deb6db7c7db551ab
Parzival Yuenyang <krumii.it@gmail.com>
2022-05-25 09:25:29 +0700

    update sync

--- app/Http/Controllers/SerialNoTriggerController.php
+++ app/Http/Controllers/SerialNoTriggerController.php
@@ -201,107 +201,107 @@ class SerialNoTriggerController extends Controller
         }
 
         try {
-            $whs = Whs::where('name', $request->whs)->first();
-            $factory = FactoryType::where('name', $request->factory)->first();
-            $part = Part::where('no', $request->part_no)->first();
-            if ($part == null) {
-                $part = new Part();
-                $part->name = $request->part_no;
-            }
+            // $whs = Whs::where('name', $request->whs)->first();
+            // $factory = FactoryType::where('name', $request->factory)->first();
+            // $part = Part::where('no', $request->part_no)->first();
+            // if ($part == null) {
+            //     $part = new Part();
+            //     $part->name = $request->part_no;
+            // }
 
-            $part->no = $request->part_no;
-            $part->save();
+            // $part->no = $request->part_no;
+            // $part->save();
 
-            $ledger = Ledger::where('part_id', $part->id)->where('factory_id', $factory->id)->where('whs_id', $whs->id)->first();
-            if ($ledger == null) {
-                $ledger = new Ledger();
-            }
-            $ledger->factory_id = $factory->id;
-            $ledger->whs_id = $whs->id;
-            $ledger->part_id = $part->id;
-            $ledger->is_active = true;
-            $ledger->save();
+            // $ledger = Ledger::where('part_id', $part->id)->where('factory_id', $factory->id)->where('whs_id', $whs->id)->first();
+            // if ($ledger == null) {
+            //     $ledger = new Ledger();
+            // }
+            // $ledger->factory_id = $factory->id;
+            // $ledger->whs_id = $whs->id;
+            // $ledger->part_id = $part->id;
+            // $ledger->is_active = true;
+            // $ledger->save();
 
-            ### create stock data
-            $stock = Stock::where('ledger_id', $ledger->id)->first();
-            if ($stock == null) {
-                $stock = new Stock();
-            }
+            // ### create stock data
+            // $stock = Stock::where('ledger_id', $ledger->id)->first();
+            // if ($stock == null) {
+            //     $stock = new Stock();
+            // }
 
-            $stock->ledger_id =  $ledger->id;
-            $stock->per_qty = $request->std_pack_qty;
-            $stock->ctn =  $request->on_stock_ctn;
-            $stock->is_active = true;
-            $stock->save();
-            LogActivity::addToLog($this->sub, 'Update Stock ' . $stock->id);
+            // $stock->ledger_id =  $ledger->id;
+            // $stock->per_qty = $request->std_pack_qty;
+            // $stock->ctn =  $request->on_stock_ctn;
+            // $stock->is_active = true;
+            // $stock->save();
+            // LogActivity::addToLog($this->sub, 'Update Stock ' . $stock->id);
 
-            ### check location
-            $location = Location::where('name', $request->shelve)->first();
-            if ($location == null) {
-                $location = new Location();
-            }
+            // ### check location
+            // $location = Location::where('name', $request->shelve)->first();
+            // if ($location == null) {
+            //     $location = new Location();
+            // }
 
-            $location->name = $request->shelve;
-            $location->description = '-';
-            $location->is_active = true;
-            $location->save();
-            LogActivity::addToLog($this->sub, 'Update Location ' . $location->id);
+            // $location->name = $request->shelve;
+            // $location->description = '-';
+            // $location->is_active = true;
+            // $location->save();
+            // LogActivity::addToLog($this->sub, 'Update Location ' . $location->id);
 
-            ### check carton
-            if (Str::substr($request->invoice_no, 0, 1) != 'S') {
-                $receive = Receive::where('whs_id', $whs->id)->where('factory_type_id', $factory->id)->where('receive_no', $request->invoice_no)->first();
-                if ($receive == null) {
-                    $receive = new Receive();
-                }
-                $receive->whs_id = $whs->id;
-                $receive->factory_type_id = $factory->id;
-                $receive->receive_date = $request->rec_date;
-                $receive->transfer_out_no = $request->invoice_no;
-                $receive->receive_no = $request->invoice_no;
-                $receive->receive_sync = true;
-                $receive->is_active = true;
-                $receive->save();
+            // ### check carton
+            // if (Str::substr($request->invoice_no, 0, 1) != 'S') {
+            //     $receive = Receive::where('whs_id', $whs->id)->where('factory_type_id', $factory->id)->where('receive_no', $request->invoice_no)->first();
+            //     if ($receive == null) {
+            //         $receive = new Receive();
+            //     }
+            //     $receive->whs_id = $whs->id;
+            //     $receive->factory_type_id = $factory->id;
+            //     $receive->receive_date = $request->rec_date;
+            //     $receive->transfer_out_no = $request->invoice_no;
+            //     $receive->receive_no = $request->invoice_no;
+            //     $receive->receive_sync = true;
+            //     $receive->is_active = true;
+            //     $receive->save();
 
-                ### check receive detail
-                $receive_detail = ReceiveDetail::where('receive_id', $receive->id)->where('ledger_id', $ledger->id)->first();
-                if ($receive_detail == null) {
-                    $receive_detail = new ReceiveDetail();
-                }
-                $receive_detail->receive_id = $receive->id;
-                $receive_detail->ledger_id = $ledger->id;
-                $receive_detail->managing_no = $request->rvn_no;
-                $receive_detail->seq += 1;
-                // $receive_detail->plan_qty = 0;
-                // $receive_detail->plan_ctn += 1;
-                $receive_detail->is_active = true;
-                $receive_detail->save();
+            //     ### check receive detail
+            //     $receive_detail = ReceiveDetail::where('receive_id', $receive->id)->where('ledger_id', $ledger->id)->first();
+            //     if ($receive_detail == null) {
+            //         $receive_detail = new ReceiveDetail();
+            //     }
+            //     $receive_detail->receive_id = $receive->id;
+            //     $receive_detail->ledger_id = $ledger->id;
+            //     $receive_detail->managing_no = $request->rvn_no;
+            //     $receive_detail->seq += 1;
+            //     // $receive_detail->plan_qty = 0;
+            //     // $receive_detail->plan_ctn += 1;
+            //     $receive_detail->is_active = true;
+            //     $receive_detail->save();
 
-                ### check carton data
-                $carton = Carton::where('serial_no', $request->serial_no)->first();
-                if ($carton == null) {
-                    $carton = new Carton();
-                    $carton->receive_detail_id = $receive_detail->id;
-                }
+            //     ### check carton data
+            //     $carton = Carton::where('serial_no', $request->serial_no)->first();
+            //     if ($carton == null) {
+            //         $carton = new Carton();
+            //         $carton->receive_detail_id = $receive_detail->id;
+            //     }
 
-                $carton->lot_no = $request->lot_no;
-                $carton->serial_no = $request->serial_no;
-                $carton->die_no = $request->case_id;
-                $carton->qty = $request->std_pack_qty;
-                $carton->is_active = true;
-                $carton->save();
+            //     $carton->lot_no = $request->lot_no;
+            //     $carton->serial_no = $request->serial_no;
+            //     $carton->die_no = $request->case_id;
+            //     $carton->qty = $request->std_pack_qty;
+            //     $carton->is_active = true;
+            //     $carton->save();
 
-                ### check shelve
-                $shelve = Shelve::where('carton_id', $carton->id)->where('location_id', $location->id)->first();
-                if ($shelve == null) {
-                    $shelve = new Shelve();
-                }
-                $shelve->carton_id = $carton->id;
-                $shelve->location_id = $location->id;
-                $shelve->pallet_no = $request->pallet_no;
-                $shelve->is_printed = false;
-                $shelve->is_active = true;
-                $shelve->save();
-            }
+            //     ### check shelve
+            //     $shelve = Shelve::where('carton_id', $carton->id)->where('location_id', $location->id)->first();
+            //     if ($shelve == null) {
+            //         $shelve = new Shelve();
+            //     }
+            //     $shelve->carton_id = $carton->id;
+            //     $shelve->location_id = $location->id;
+            //     $shelve->pallet_no = $request->pallet_no;
+            //     $shelve->is_printed = false;
+            //     $shelve->is_active = true;
+            //     $shelve->save();
+            // }
 
             $data = new SerialNoTrigger();
             $data->invoice_no = $request->invoice_no;
